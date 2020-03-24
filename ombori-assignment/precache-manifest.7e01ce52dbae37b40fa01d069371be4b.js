ad8141debdb97d6c8e2328d5f2298f5796aac739
Usman Tahir <usman@droobihealth.com>
2020-03-25 03:07:00 +0500

    added build

---
+++ ombori-assignment/precache-manifest.7e01ce52dbae37b40fa01d069371be4b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "192704882057a64c950f1467c060f48b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e5991e11ba344cc24e3",
+    "url": "/static/css/3.c3cab70b.chunk.css"
+  },
+  {
+    "revision": "68a86ecdd7a3bcbf7e9b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e793ad0d18624ec79579",
+    "url": "/static/js/0.1e5ebcc0.chunk.js"
+  },
+  {
+    "revision": "0e5991e11ba344cc24e3",
+    "url": "/static/js/3.965426d0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/3.965426d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43d1cdcae17d1836407a",
+    "url": "/static/js/4.f33780da.chunk.js"
+  },
+  {
+    "revision": "ae56bf0dec741614386a",
+    "url": "/static/js/5.9fb87da3.chunk.js"
+  },
+  {
+    "revision": "21077188c44e9454772e",
+    "url": "/static/js/6.9015bc34.chunk.js"
+  },
+  {
+    "revision": "48328dd739caf6731e99",
+    "url": "/static/js/7.ee025743.chunk.js"
+  },
+  {
+    "revision": "68a86ecdd7a3bcbf7e9b",
+    "url": "/static/js/main.122e9f2a.chunk.js"
+  },
+  {
+    "revision": "46f75ecd1f8cef77fd63",
+    "url": "/static/js/runtime-main.0fb05a06.js"
+  }
+]);
